a12501e7879a561fdab8204e6783cfe402f84705
vaibhav <vaibhavbansode855@gmail.com>
2022-02-26 19:46:02 +0530

    assignment book

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,8 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createNewBook", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBookList", UserController.getUsersData)
 
 module.exports = router;
