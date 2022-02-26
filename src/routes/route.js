3c742302f56f77cca89f5818197fa74e4bb8a9bb
vaibhav <vaibhavbansode855@gmail.com>
2022-02-26 22:01:19 +0530

    getassignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const router = express.Router();
+//1
+router.get('/movies', function(req, res) {
+    
+    res.send('["fukrey","kotafactory"]')
+})
+console.log()
+//2
+router.get('/movies/:movieId', function(req, res) {
+    movie =["fukrey","nawabzade","boothnath"]
+    let value = req.params.movieId.length
+    if(value>movie.length-1){
+        res.send("doesn't exist")
+    }
+    else{
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+        res.send(movie[value])
+    }
+});
+//3
+router.get('/films', function(req, res) { res.send
+    [ {
+        "i": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Demo"
+       }]
+       
+
 })
 
 
