3374d060621dac0d4389357fa6f69634c79bda2d
vaibhav <vaibhavbansode855@gmail.com>
2022-03-01 16:40:48 +0530

    assignment mongoDb

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,25 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        required: true,
+    },
     prices: {
         indianPrice: String,
-        europePrice: String,
+        europenPrice: String,
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    year: {
+        type: Number,
+        default: 2021,
+    },
+    tags: [String],
+    authorName: String,
+    totalPages: Number,
+    stockAvailable: Boolean,
+}, {
+    timestamps: true
+});
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+module.exports = mongoose.model('Book', bookSchema) 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,19 @@
 const express = require('express');
+
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
+router.get("/bookData", BookController.bookData  )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/booksList", BookController.booksList)
+router.get("/getBooksInYear", BookController.getBooksInYear)
+router.get("/getParticularBooks", BookController.getParticularBooks)
+router.get("/getXINRBooks", BookController.getXINRBooks)
+router.get("/getRandomBooks", BookController.getRandomBooks)
+
+
+
 
 module.exports = router;
