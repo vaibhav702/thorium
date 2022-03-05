2f428c49099758203dee112fd62948c30e1b3775
vaibhav <vaibhavbansode855@gmail.com>
2022-03-05 20:33:44 +0530

    assignment middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const requestIp = require("request-ip");
+const moment = require('moment')
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
+app.use(requestIp.mw({attributeName:'customIp'}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req,res,next) =>{
+    let date = moment().format('MM Do YYYY','h:mm:ss a')
+
+    let ip =  req.CustomIp;
+    let url = req.url;
+    console.log(date,ip,url)
+    next()
+    
+})
 
 
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
